f76782b09d64f0fe101fd829e549d3b1405dbe3d
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-12-15 23:25:47 +0530

    client:fix: load server and host from appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-﻿using TcpStatusClient;
+﻿using Microsoft.Extensions.Configuration;
+using TcpStatusClient;
 
 
 class Program
 {
     static async Task Main(string[] args)
     {
+        // laod configurations
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        // read connection details
+        var host = config["Server:Host"] ?? throw new InvalidOperationException("Server:Host not configured");
+        var portStr = config["Server:Port"] ?? throw new InvalidOperationException("Server:Port not configured");
+        var port = int.Parse(portStr);
+
         // read client name from terminal
         var clientName = args.Length > 0 ? args[0] : "Client";
 
-        var client = new StatusClient("127.0.0.1", 5000, clientName);
+        var client = new StatusClient(host, port, clientName);
         await client.StartAsync();
     }
 }
